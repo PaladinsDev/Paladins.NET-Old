9edc42d787f4a83377a8f620a868d62a9ee9f2f6
Matthew Hatcher <matthewh@halfpetal.com>
2019-03-12 00:59:14 -0400

    Some more changes.

--- Paladins.NET/API.cs
+++ Paladins.NET/API.cs
@@ -8,7 +8,28 @@ namespace PaladinsDev.PaladinsDotNET
 {
     public class API
     {
+        private static API instance;
+
         private string DevId;
         private string AuthKey;
+        private int LanguageId;
+        private Cache cache;
+
+        public API(string id, string key)
+        {
+            this.DevId = id;
+            this.AuthKey = key;
+            this.LanguageId = 1;
+        }
+
+        public static API Instance(string id, string key)
+        {
+            if (instance == null)
+            {
+                instance = new API(id, key);
+            }
+
+            return instance;
+        }
     }
 }
---
+++ Paladins.NET/Variables.cs
@@ -0,0 +1,8 @@
+﻿namespace PaladinsDev.PaladinsDotNET
+{
+    class Variables
+    {
+        public static string API_URL = "http://api.paladins.com/paladinsapi.svc";
+        public static string CACHE_SESSION_ID = "paladinsdotnet.sessionId";
+    }
+}
